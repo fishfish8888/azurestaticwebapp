434056e5805db119b448a146b6431ddcef8949fd
fishfish8888 <42741164+fishfish8888@users.noreply.github.com>
2023-01-12 17:01:27 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,15 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
+import FancyParagraph from "../components/fancy-paragraph"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
-const AboutPage = () => <h1>About Me</h1>
+const AboutPage = () => (
+<Layout>
+  <h1>About Me</h1>
+  <FancyParagraph paragraphText="Styled with CSS Modules." />
+</Layout>
+)
 
 export default AboutPage
