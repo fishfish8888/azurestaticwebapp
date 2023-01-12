8b901adb589a65f19dcfb681192034fac10865f3
fishfish8888 <42741164+fishfish8888@users.noreply.github.com>
2023-01-12 16:13:50 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ const IndexPage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1>
-        Welcome to <b>Gatsby!</b>
+        <p>Welcome to my SitePoint demo Site!</p>
       </h1>
       <p className={styles.intro}>
         <b>Example pages:</b>{" "}
