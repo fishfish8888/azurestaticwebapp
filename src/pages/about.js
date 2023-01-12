d6da6a5a05c8877b7757afdf584cba28d2138861
fishfish8888 <42741164+fishfish8888@users.noreply.github.com>
2023-01-12 16:41:47 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,10 @@
-import React from "react"
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
 
 const AboutPage = () => <h1>About Me</h1>
 
