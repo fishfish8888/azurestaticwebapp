7cd6e6b80a556ae9a4df1557bd54bd14d25d9f51
fishfish8888 <42741164+fishfish8888@users.noreply.github.com>
2023-01-13 15:23:21 +0800

    Update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,44 +1,64 @@
-import { useState, useEffect } from 'react';
+import "../components/App.css";
+import { useState } from "react";
 
-const App = () => {
-   const [posts, setPosts] = useState([]);
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [message, setMessage] = useState("");
 
-   useEffect(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-         .then((res) => res.json())
-         .then((data) => {
-            console.log(data);
-            setPosts(data);
-         })
-         .catch((err) => {
-            console.log(err.message);
-         });
-   }, []);
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          mobileNumber: mobileNumber,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setName("");
+        setEmail("");
+        setMessage("User created successfully");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-   return (
-      <>
-         <div className="add-post-container">
-            <form>
-               <input type="text" className="form-control" />
-               <textarea className="form-control" cols="10" rows="8"></textarea>
-               <button type="submit">Add Post</button>
-            </form>
-         </div>
-         <div className="posts-container">
-            {posts.map((post) => {
-               return (
-                  <div className="post-card" key={post.id}>
-                     <h2 className="post-title">{post.title}</h2>
-                     <p className="post-body">{post.body}</p>
-                     <div className="button">
-                        <div className="delete-btn">Delete</div>
-                     </div>
-                  </div>
-               );
-            })}
-         </div>
-      </>
-   );
-};
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          value={mobileNumber}
+          placeholder="Mobile Number"
+          onChange={(e) => setMobileNumber(e.target.value)}
+        />
+
+        <button type="submit">Create</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
+    </div>
+  );
+}
 
 export default App;
