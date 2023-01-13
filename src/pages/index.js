d0c978a64480ac56f652c6d8eda0cb92927c4cdd
fishfish8888 <42741164+fishfish8888@users.noreply.github.com>
2023-01-13 11:13:49 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 import InputForm from "../components/InputForm";
 
 import Layout from "../components/layout"
