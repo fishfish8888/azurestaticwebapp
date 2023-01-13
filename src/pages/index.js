4a6270813f82ac0d254e14746eb900fab30e2450
fishfish8888 <42741164+fishfish8888@users.noreply.github.com>
2023-01-13 10:24:33 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,17 +38,6 @@ const IndexPage = () => (
       <h1>
         <p>Welcome to my SitePoint demo Site!</p>
       </h1>
-      <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
-        {samplePageLinks.map((link, i) => (
-          <React.Fragment key={link.url}>
-            <Link to={link.url}>{link.text}</Link>
-            {i !== samplePageLinks.length - 1 && <> · </>}
-          </React.Fragment>
-        ))}
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p>
     </div>
       <div className="pt-6">
         <ContactForm />
