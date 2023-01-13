e97f3f6bc2641414507b2202f4150d281862da5f
fishfish8888 <42741164+fishfish8888@users.noreply.github.com>
2023-01-13 10:23:00 +0800

    Update ContactForm.js

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -31,8 +31,6 @@ const ContactForm = () => {
           type="text"
           placeholder="Site domain name, e.g. www.example.com"
           name="hostname"
-#          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-#          required
         />
       </div>
       <div className="mb-3 pt-0">
@@ -40,13 +38,10 @@ const ContactForm = () => {
           type="text"
           placeholder="AKS namespace"
           name="namespace"
-#          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-#          required
         />
       </div>
       <div className="mb-3 pt-0">
         <button
-#          className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
           type="submit"
         >
           Submit
