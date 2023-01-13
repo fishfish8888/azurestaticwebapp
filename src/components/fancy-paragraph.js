d4e4d9fff5c942cac5f2bb3b901ba2b31f9b9d30
fishfish8888 <42741164+fishfish8888@users.noreply.github.com>
2023-01-13 09:21:21 +0800

    Update fancy-paragraph.js

--- src/components/fancy-paragraph.js
+++ src/components/fancy-paragraph.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { fancy } from "./fancy-paragraph.module.css"
 
-const FancyParagraph = (props) => <p className={fancy}>{props.paragraphText}</p>
+const FancyParagraph = ({ props }) => <p className={fancy}>{props.paragraphText}</p>
 
 export default FancyParagraph
