af4e578a52c0173b6c87c95de2a0f95dcaa4477d
fishfish8888 <42741164+fishfish8888@users.noreply.github.com>
2023-01-13 10:07:21 +0800

    Update ContactForm.js

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -29,25 +29,17 @@ const ContactForm = () => {
       <div className="mb-3 pt-0">
         <input
           type="text"
-          placeholder="Your name"
-          name="name"
+          placeholder="Site domain name, e.g. www.example.com"
+          name="host"
           className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
           required
         />
       </div>
       <div className="mb-3 pt-0">
         <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-          required
-        />
-      </div>
-      <div className="mb-3 pt-0">
-        <textarea
-          placeholder="Your message"
-          name="message"
+          type="text"
+          placeholder="AKS namespace"
+          name="namespace"
           className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
           required
         />
@@ -57,7 +49,7 @@ const ContactForm = () => {
           className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
           type="submit"
         >
-          Send a message
+          Submit
         </button>
       </div>
     </form>
