129dae7aafcf355491745efeb4791fcf265f90ce
fishfish8888 <42741164+fishfish8888@users.noreply.github.com>
2023-01-13 14:52:37 +0800

    Update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -6,15 +6,15 @@ import Form from "../components/Form";
 function App() {
   // You can skip useEffect if you're not using TailwindCSS
   // Otherwise, for the production usage refer to https://tailwindcss.com/docs/installation
-  useEffect(() => {
-    if (document) {
-      const stylesheet = document.createElement("link");
-      stylesheet.rel = "stylesheet";
-      stylesheet.href = "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
-
-      document.head.appendChild(stylesheet);
-    }
-  }, []);
+#  useEffect(() => {
+#    if (document) {
+#      const stylesheet = document.createElement("link");
+#      stylesheet.rel = "stylesheet";
+#      stylesheet.href = "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
+#
+#      document.head.appendChild(stylesheet);
+#    }
+#  }, []);
 
   return (
     <div className="App">
